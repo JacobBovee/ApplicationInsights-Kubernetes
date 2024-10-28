e4d0436766ff28d88d13d93dda7b344cf3ad0a1f
Jacob Bovee (from Dev Box) <jabovee@microsoft.com>
2024-10-28 14:20:01 -0700

    Add generated scrubber fix

--- src/ApplicationInsights.Kubernetes/Utilities/Scrubber.cs
+++ src/ApplicationInsights.Kubernetes/Utilities/Scrubber.cs
@@ -14,16 +14,19 @@ namespace Microsoft.ApplicationInsights.Kubernetes
     public class Scrubber
     {
         public const string EmailRegExPattern = @"[a-zA-Z0-9!#$+\-^_~]+(?:\.[a-zA-Z0-9!#$+\-^_~]+)*@(?:[a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,6}";
+
         public static string ScrubData(string data, char replacementChar)
         {
             Regex rx = new Regex(EmailRegExPattern);
+            StringBuilder result = new StringBuilder(data);
+
             foreach (Match match in rx.Matches(data))
             {
                 string replacementString = new string(replacementChar, match.Value.Length);
-                data = data.Replace(match.Value, replacementString);
+                result.Replace(match.Value, replacementString, match.Index, match.Length);
             }
 
-            return data;
+            return result.ToString();
         }
     }
 }
